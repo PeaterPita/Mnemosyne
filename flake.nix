0259f0e171fdeaeda0a9224f7dda0bee7142e6e0
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-06-23 14:20:36 +0100

    First Commit ++ Under Construction

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Personal Site Flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+    in
+    {
+      packages.${system}.default = pkgs.buildNpmPackage {
+        pname = "Mnemosyne";
+        version = "0.1.0";
+
+        src = ./.;
+        npmDepsHash = "";
+
+        buildPhase = ''
+          npx astro build
+        '';
+
+        installPhase = ''
+          cp -r dist $out
+        '';
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+        ];
+      };
+    };
+}
