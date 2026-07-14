cbad4b5024faaaf7a8f7f1e3a5865752f74f5673
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-07-14 14:06:32 +0100

    Tweaks ++ POSTCSS with cssnano ++ constellation is enum checked to avoid mistypes ++ MainLayout now takes miniumum 100vh with flex 1 to push footer to the bottom

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
+import { constellations } from "./data/constellations";
 
 const blogs = defineCollection({
 
@@ -12,7 +13,7 @@ const blogs = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        constellation: z.string(),
+        constellation: z.enum(constellations.flatMap(constellation => constellation.name)),
         tags: z.array(z.string()),
         date: z.coerce.date()
     })
