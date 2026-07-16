242d82e985f3abee418612d9b66b450a5998e41d
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-07-16 14:33:25 +0100

    Started Project Section ++ Added optional description to section header in prep ++ new projects.ts data file ++ Expanded projects section beyond simple stub

---
+++ src/data/projects.ts
@@ -0,0 +1,31 @@
+let PLACEHOLDER = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThe8vy1axz9uuWirx3U1FHqlkmjOQETUQF9cVet0JbW6S7euudlGqP9Dg&s=10";
+
+
+export const projects = [
+
+    {
+        name: "Mnemosyne",
+        description: "This very portfolio site",
+        repo: "https://github.com/PeaterPita/Mnemosyne",
+        tags: [],
+        image: {
+            src: PLACEHOLDER,
+            alt: "PLACEHOLDER"
+        }
+    },
+
+
+
+
+    {
+        name: "Testing",
+        description: "Hello world this is a description",
+        repo: "https://peaterpita.com",
+        tags: ["Rust"],
+        image: {
+            src: PLACEHOLDER,
+            alt: "PLACEHOLDER",
+        }
+    },
+
+];
